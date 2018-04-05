83f61660faf18e45d612015de4f176c693a3681c
Fabian Martinez Portantier <fabian@portantier.com>
2018-04-05 16:11:46 -0300

    fix

--- asydns-dnsd.py
+++ asydns-dnsd.py
@@ -8,16 +8,6 @@ from pathlib import Path
 from dnslib import QTYPE, RR, A
 from dnslib.server import BaseResolver, DNSServer
 
-NAME_VALID_FOR = 3600
-
-user = pwd.getpwnam(sys.argv[1])
-
-datadir = Path(user.pw_dir) / '.asydns' / 'data'
-
-datadir.mkdir(parents=True, exist_ok=True)
-
-regex_sha224 = re.compile('[0-9a-f]{56}')
-
 
 def drop_privileges(new_user):
     if os.getuid() != 0:
@@ -40,14 +30,51 @@ def drop_privileges(new_user):
 
 class AsymResolver(BaseResolver):
 
+    def __init__(self):
+
+        user = pwd.getpwnam(sys.argv[1])
+
+        self.homedir = Path(user.pw_dir)
+        self.dotdir = homedir / '.asydns'
+        self.datadir = dotdir / 'data'
+
+        self.datadir.mkdir(parents=True, exist_ok=True)
+
+        self.regex_sha224 = re.compile('[0-9a-f]{56}')
+
+        self.cfg_file = dotdir / 'config.json'
+
+        defaults = {
+            'domain': 'a.asydns.org',
+            'ttl' : 3600,
+            'registers': {}
+        }
+
+        self.cfg = defaults
+
+        if self.cfg_file.is_file():
+            try:
+                with self.cfg_file.open() as c:
+                    self.cfg.update(json.loads(c.read()))
+            except Exception:
+                print('error loading config file, using defaults', file=sys.stderr)
+
+
     def resolve(self, request, handler):
         reply = request.reply()
         qname = request.q.qname
         qn = str(qname)
 
-        if regex_sha224.match(qn.split('.')[0]):
+        print(request.q)
+        print(dir(request.q))
+        print(qname)
+        if qname in self.cfg['registers'].keys():
+            answer = RR(qname, QTYPE.A, rdata=A(registers[qname]), ttl=300)
+            reply.add_answer(answer)
+
+        if self.regex_sha224.match(qn.split('.')[0]):
             sha224 = qn.split('.')[0]
-            ip_file = datadir / sha224
+            ip_file = self.datadir / sha224
 
             if ip_file.is_file() and (time.time() - ip_file.stat().st_mtime) < NAME_VALID_FOR:
                 with ip_file.open() as ipf:
@@ -73,3 +100,4 @@ if __name__ == "__main__":
 
     while server.isAlive():
         time.sleep(1)
+
